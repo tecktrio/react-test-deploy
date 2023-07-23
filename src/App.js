d68248ead37262bf3dcd82dad096971a471176f9
thisisamal <amalpullan4@gmail.com>
2023-07-23 18:55:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from './Home';
 import About from './About';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route path='/react-test-deploy/about' element={<About/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
