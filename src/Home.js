85f43772cd81114316092f76dfbb61ff11e40729
thisisamal <amalpullan4@gmail.com>
2023-07-23 19:04:20 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
       hello
-      
+<Link to='about' >about</Link>
     </div>
   )
 }
